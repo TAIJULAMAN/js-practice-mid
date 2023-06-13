6e321e24bd1ddb2901c4d54aec065511c945277d
TAIJULAMAN <taijulaman8@gmail.com>
2023-06-13 23:21:24 +0600

    js practice

---
+++ js-practice.js
@@ -0,0 +1,57 @@
+// ১. একটা কোড লিখো। যেটা দিয়ে কোন একটা array এর মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিতে পারবে। 
+var numbers=[5,6,11,44,3,4];
+
+function SmallestNumber(numbers){
+    
+    let smallest =numbers[0];
+    for (let i = 0 ; i < numbers.length; i++) {
+
+    var element = numbers[i];
+
+      if (element < smallest) {
+        smallest = element;
+      }
+    }
+    
+    return smallest;
+  }
+  smallest =SmallestNumber(numbers);
+  var result = SmallestNumber(numbers);
+//   console.log(result);
+
+// ২. একটা কোড লিখো যেটা দিয়ে তিনটা সংখ্যার মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিবে।  
+
+var numbers =[12,11,10];
+function searchingSmallNumber(numbers){
+    var smallNumber = numbers[0];
+    for(var i= 0; i<numbers.length; i++){
+        var element = numbers[i];
+        if(element < smallNumber)
+        {
+            smallNumber= element;
+        }
+       
+    }
+    return smallNumber;
+}
+smallNumber=searchingSmallNumber(numbers);
+ var result=searchingSmallNumber(numbers);
+//  console.log(result);
+
+// ৩. একটা ফাংশন লিখো। 
+// সেই ফাংশনের মধ্যে ইনপুট হিসেবে একটা array নিবে। 
+// সেই array এর মধ্যে অনেকগুলা সংখ্যা থাকবে।
+//  তোমার কাজ হবে ইনপুট নেয়া array এর মধ্যে যতগুলা সংখ্যা আছে। সেই সংখ্যা গুলার গড় বের করবে। তারপর সেই গড় ফাংশনের রিটার্ন হিসেবে দিয়ে দিবে। 
+
+function calculateAverage(number) {
+    let sum = 0;
+    for (let i = 0; i < number.length; i++) {
+      sum += number[i];
+    }
+  
+    return sum /number.length;
+  }
+  
+  var numbers = [4, 7, 12, 3, 9];
+  var average = calculateAverage(numbers);
+  console.log(average); // Output: 7
